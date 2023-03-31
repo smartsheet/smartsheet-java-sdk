c8cb04faabcc07a013ec5f4b4fe74a009d2709f5
Austin Pederson <austin.pederson@smartsheet.com>
2023-03-31 10:49:47 -0700

    remove unnecessary import

--- src/main/java/com/smartsheet/api/internal/http/interceptor/HeadersInterceptor.java
+++ src/main/java/com/smartsheet/api/internal/http/interceptor/HeadersInterceptor.java
@@ -20,7 +20,6 @@ package com.smartsheet.api.internal.http.interceptor;
  * %[license]
  */
 
-import com.smartsheet.api.Smartsheet;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
