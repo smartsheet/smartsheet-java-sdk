8b9e6376f8091550fc9c50ada0b776c5d97dcb17
Priyanka Lakhe <priyanka.lakhe@smartsheet.com>
2023-07-20 17:17:38 -0700

    code review - updated import 2 (missed one)

--- src/test/java/com/smartsheet/api/internal/SheetResourcesImplTest.java
+++ src/test/java/com/smartsheet/api/internal/SheetResourcesImplTest.java
@@ -64,7 +64,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class SheetResourcesImplTest extends ResourcesImplBase {
     private SheetResourcesImpl sheetResource;
