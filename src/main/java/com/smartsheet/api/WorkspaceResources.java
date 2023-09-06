f6a2113be1f88a3853cf208459aaed49341d0cd2
zromano <zachary.c.romano@gmail.com>
2023-09-05 23:02:35 -0700

    Add copyWorkspace

--- src/main/java/com/smartsheet/api/WorkspaceResources.java
+++ src/main/java/com/smartsheet/api/WorkspaceResources.java
@@ -164,9 +164,9 @@ public interface WorkspaceResources {
      * @param excludes optional parameters to exclude
      * @return the folder
      * @throws SmartsheetException the smartsheet exception
+     * @deprecated As of release 2.0. `excludes` param is deprecated. Please use the `copyWorkspace` method with `includes` instead.
      */
     @Deprecated(since = "2.0.0", forRemoval = true)
-    // todo why is this deprecated?
     Workspace copyWorkspace(long workspaceId, ContainerDestination containerDestination, EnumSet<WorkspaceCopyInclusion> includes,
                                    EnumSet<WorkspaceRemapExclusion> skipRemap, EnumSet<CopyExclusion> excludes) throws SmartsheetException;
 
