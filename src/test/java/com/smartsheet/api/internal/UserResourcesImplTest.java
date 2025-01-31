c305006930574ae26595151c4ba5124fbd46f5a8
Zach Romano <zachary.c.romano@gmail.com>
2025-01-31 10:04:49 -0800

    Fix imports

--- src/test/java/com/smartsheet/api/internal/UserResourcesImplTest.java
+++ src/test/java/com/smartsheet/api/internal/UserResourcesImplTest.java
@@ -18,14 +18,14 @@ package com.smartsheet.api.internal;
 
 import com.smartsheet.api.SmartsheetException;
 import com.smartsheet.api.internal.http.DefaultHttpClient;
-import com.smartsheet.api.models.Account;
-import com.smartsheet.api.models.AlternateEmail;
-import com.smartsheet.api.models.DeleteUserParameters;
-import com.smartsheet.api.models.PagedResult;
 import com.smartsheet.api.models.PaginationParameters;
-import com.smartsheet.api.models.Sheet;
 import com.smartsheet.api.models.User;
+import com.smartsheet.api.models.PagedResult;
 import com.smartsheet.api.models.UserProfile;
+import com.smartsheet.api.models.Account;
+import com.smartsheet.api.models.DeleteUserParameters;
+import com.smartsheet.api.models.Sheet;
+import com.smartsheet.api.models.AlternateEmail;
 import com.smartsheet.api.models.enums.ListUserInclusion;
 import com.smartsheet.api.models.enums.UserInclusion;
 import com.smartsheet.api.models.enums.UserStatus;
@@ -33,15 +33,14 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.EnumSet;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
